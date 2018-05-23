f900c47c116dc33f39d24b212a9f5b34ed715573
munirkizilcali <munir.kizilcali@gmail.com>
2018-05-23 17:16:45 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: {minimum: 10}
+	validate :invalid_category
+
+	def invalid_category
+		values = ["Fiction", "Non-Fiction"]
+		result = false
+		values.each do |value|
+			if value == self.category 
+				result = true
+			end
+		end
+		if result == false
+			errors.add(:category, "No valid category name")
+		end
+		result
+	end
 end
